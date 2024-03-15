43d83705d558a9373b1e51ca7e4fcff0cc501519
Baraa Al-Masri <pub@mbaraa.com>
2024-03-15 19:00:42 +0300

    chore: add json-enc for htmx

---
+++ blog/setting-up-go-templ-with-tailwind-htmx-docker/static/js/json-enc.js
@@ -0,0 +1,19 @@
+htmx.defineExtension("json-enc", {
+  onEvent: function (name, evt) {
+    if (name === "htmx:configRequest") {
+      evt.detail.headers["Content-Type"] = "application/json";
+    }
+  },
+
+  encodeParameters: function (xhr, parameters, elt) {
+    xhr.overrideMimeType("text/json");
+    for (const key in parameters) {
+      const tryNum = parseFloat(parameters[key]);
+      // using == to check only the value against the string
+      if (parameters[key] == tryNum) {
+        parameters[key] = tryNum;
+      }
+    }
+    return JSON.stringify(parameters);
+  },
+});
