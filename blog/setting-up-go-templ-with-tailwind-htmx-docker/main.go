ffd1eac4547cfd9c7b8307ca1a47c096ddaa444f
Baraa Al-Masri <pub@mbaraa.com>
2024-03-15 16:25:47 +0300

    chore: udpate routes in main

--- blog/setting-up-go-templ-with-tailwind-htmx-docker/main.go
+++ blog/setting-up-go-templ-with-tailwind-htmx-docker/main.go
@@ -1,22 +1,54 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"log"
 	"net/http"
 	"spendings/components"
-
-	"github.com/a-h/templ"
+	"spendings/db"
+	"spendings/handlers"
+	"spendings/services"
+	"spendings/session"
 )
 
 //go:embed static/*
 var static embed.FS
 
+//go:generate npx tailwindcss build -i static/css/style.css -o static/css/tailwind.css -m
+
 func main() {
-	homePage := components.Index()
+	session.PrintPassword()
+
+	ctx := context.Background()
+
+	balanceStore := db.NewBalanceStoreJson()
+	spendingsStore := db.NewSpendingsStoreJson()
+	balanceService := services.NewBalanceService(balanceStore)
+	spendingsService := services.NewSpendingService(spendingsStore)
+
 	pagesHandler := http.NewServeMux()
-	pagesHandler.Handle("/", templ.Handler(homePage))
+	pagesHandler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		spendings, err := spendingsService.ListItems()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		components.Index(balanceService.GetBalance(), spendings).Render(ctx, w)
+	})
 	pagesHandler.Handle("/static/", http.FileServer(http.FS(static)))
 
-	log.Fatalln(http.ListenAndServe(":8080", pagesHandler))
+	spendingsHandler := handlers.NewSpendingHandler(*spendingsService)
+	restHandler := http.NewServeMux()
+	restHandler.HandleFunc("POST /spending", spendingsHandler.HandleAddSpendingItem)
+	restHandler.HandleFunc("PUT /spending", spendingsHandler.HandleUpdateSpendingItem)
+	restHandler.HandleFunc("DELETE /spending", spendingsHandler.HandleRemoveSpendingItem)
+
+	applicationHandler := http.NewServeMux()
+	applicationHandler.Handle("/", pagesHandler)
+	applicationHandler.Handle("/api/", http.StripPrefix("/api", restHandler))
+
+	log.Println("starting server on port 8080")
+	log.Fatalln(http.ListenAndServe(":8080", applicationHandler))
 }
